eb4e1f3bc6691e55b2ec229d0eea0f8600dacf78
KyuDan1 <116885375+KyuDan1@users.noreply.github.com>
2023-08-16 17:29:59 +0900

    Update extract_purified_transcript.py

--- extract_purified_transcript.py
+++ extract_purified_transcript.py
@@ -1,3 +1,5 @@
+import os
+
 def get_lines_list(text_path):
     with open(text_path, 'r') as f:
         lines = f.readlines()
@@ -23,15 +25,6 @@ def get_transcript_txt(input_text_path):
         result.append(temp2)
     return result
 
-# 주어진 텍스트 파일의 경로와 변환한 시간 정보를 저장할 파일의 경로
-input_text_path = 'transcript/번역- 1. Orientation.txt'
-output_time_info_path = 'before_transcript/01.txt'
-
-
-transcripts = get_transcript_txt(input_text_path)
-
-#중복 내용 삭제 전
-list_to_txt(transcripts,output_time_info_path)
 
 def remove_duplicate_sentences(list):
     result = []
@@ -41,7 +34,14 @@ def remove_duplicate_sentences(list):
      
     return result
 
-deleted_output = 'after_transcript/01.txt'
 
 #중복 내용 삭제 후
-list_to_txt(remove_duplicate_sentences(transcripts),deleted_output)
+#list_to_txt(remove_duplicate_sentences(transcripts),deleted_output)
+
+
+directory = 'transcript'
+for filename in os.listdir(directory):
+    f = os.path.join(directory,filename)
+    transcripts = get_transcript_txt(f)
+    list_to_txt(remove_duplicate_sentences(transcripts),f'after_transcript/purified_{filename}')
+
