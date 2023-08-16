6bf3dbdbc102de7ff8bcc42dd43bc9ad730a2a8b
KyuDan1 <116885375+KyuDan1@users.noreply.github.com>
2023-08-17 00:46:27 +0900

    Update resampling.py

--- resampling.py
+++ resampling.py
@@ -4,14 +4,28 @@ import soundfile as sf
 
 def down_sample(input_wav, origin_sr, resample_sr):
     y, sr = librosa.load(input_wav, sr=origin_sr)
-    resample = librosa.resample(y, orig_sr = sr, target_sr = resample_sr)
+    resample = librosa.resample(y, orig_sr=sr, target_sr=resample_sr)
     return resample
 
-directory = 'splitted_audio'
+upper_directory = 'splitted_audio'
+output_directory = 'resampled_splitted_audio'
 
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    resample = down_sample(f,librosa.get_samplerate(f),16000)
-    #print(librosa.get_samplerate(f))
-    sf.write(f'resampled_splitted_audio/{filename}',data=resample,samplerate=16000, format='WAV')
-    #librosa.output.write_wav('resampled_splitted_audio/{filename}', resample, 16000) # save half-cut file
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+directory = os.listdir(upper_directory)
+print(directory)
+for file in directory:
+    audios = os.listdir(os.path.join(upper_directory, file))
+    for audio in audios:
+        input_file = os.path.join(upper_directory, file, audio)
+        output_dir = os.path.join(output_directory, file)
+        
+        # Create the output subdirectory if it doesn't exist
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        
+        resample = down_sample(input_file, librosa.get_samplerate(input_file), 16000)
+        output_file = os.path.join(output_dir, audio)
+        sf.write(output_file, data=resample, samplerate=16000, format='WAV')
