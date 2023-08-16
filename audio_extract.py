bd5cb4ab63f5cd0feee550842f6954e5d089cce7
KyuDan1 <116885375+KyuDan1@users.noreply.github.com>
2023-08-17 00:11:27 +0900

    Update audio_extract.py

--- audio_extract.py
+++ audio_extract.py
@@ -1,6 +1,4 @@
 from moviepy.editor import *
-import scipy.io.wavfile as wav
-import librosa
 
 def mp4_to_wav(mp4_file_path, wav_file_path):
     try:
@@ -18,21 +16,12 @@ def mp4_to_wav(mp4_file_path, wav_file_path):
     except Exception as e:
         print(f"Error occurred during conversion: {e}")
 
-def resample_wav(input_path, output_path, target_sr):
-    # Load the original WAV file
-    data, original_sr = librosa.load(input_path, sr=None)
-    
-    # Resample the data
-    resampled_data = librosa.resample(data, original_sr, target_sr)
-    
-    # Save the resampled data as a new WAV file
-    librosa.output.write_wav(output_path, resampled_data, target_sr)
 
-
-mp4_file_path = "MoonSoomook_Advanced_compiler/1-Orientation.mp4"
-wav_file_path = "audio/audio01.wav"
-#output_file = "audio_sr16000.wav"
     
 
-mp4_to_wav(mp4_file_path, wav_file_path)
-#resample_wav(wav_file_path, output_file, 16000)
+
+directory = 'MoonSoomook_Advanced_compiler'
+for filename in os.listdir(directory):
+    f = os.path.join(directory,filename)
+    mp4_to_wav(f, f'audio/audio_{filename}'.replace("mp4","wav"))
+
