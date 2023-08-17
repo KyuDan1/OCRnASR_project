909bd57f47f7a25d77fea1d82a8da0c0c4db5a02
KyuDan1 <116885375+KyuDan1@users.noreply.github.com>
2023-08-18 02:05:03 +0900

    Update ASR_vanilla.py

--- ASR_vanilla.py
+++ ASR_vanilla.py
@@ -4,7 +4,7 @@ import numpy as np
 import librosa
 from IPython.display import Audio, display
 from plotly import graph_objects as go
-import ipywidgets
+#import ipywidgets
 
 import nemo
 import nemo.collections.asr as nemo_asr
@@ -30,13 +30,28 @@ def save_string_to_txt(filename, content):
         print(f"error")
 
 
-directory = 'resampled_splitted_audio'
+
 am_model_conformer = nemo_asr.models.ASRModel.from_pretrained(model_name="stt_en_conformer_ctc_large")
+print("loading complete".upper)
 
-output_filename = 'pure_ASR_transcript/'
+upper_directory = 'resampled_splitted_audio'
+output_directory = 'pure_ASR_transcript'
 
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    save_string_to_txt(output_filename+f'{filename}'.replace("wav","txt"),conformer_wav_to_transcript(f))
-    #with open(output_filename+f'{filename}.', 'w')
-    #print(conformer_wav_to_transcript(f))
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+directory = os.listdir(upper_directory)
+for file in directory:
+    audios = os.listdir(os.path.join(upper_directory, file))
+    for audio in audios:
+        input_file = os.path.join(upper_directory, file, audio)
+        output_dir = os.path.join(output_directory, file)
+        
+        # Create the output subdirectory if it doesn't exist
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        
+        save_string_to_txt(output_dir+'/pureASRtext '+f'{audio}'.replace("wav","txt"),conformer_wav_to_transcript(input_file))
+
+#오디오에 데이터가 없을 때는 빈 txt 파일을 만드도록 하는 코드가 필요함.
