6877a3a27f294f624bb60e58b99caadafb82c8ac
KyuDan1 <116885375+KyuDan1@users.noreply.github.com>
2023-08-18 00:14:38 +0900

    Update extract_purified_transcript.py
    transcript 원본에서 자막 내에서 줄바꿈을 하는 경우까지 커버.

--- extract_purified_transcript.py
+++ extract_purified_transcript.py
@@ -9,7 +9,7 @@ def get_lines_list(text_path):
 def list_to_txt(input_list, filename):
     with open(filename, 'w') as file:
         for item in input_list:
-            file.write(str(item) + '\n')
+            file.write(str(item) + '\n\n')
 
     
 def get_transcript_txt(input_file, pattern):
@@ -20,10 +20,10 @@ def get_transcript_txt(input_file, pattern):
     for text in lines:
         if not re.fullmatch(pattern, text):
             new_lines.append(text)
-    for list in new_lines:
+    """for list in new_lines:
         temp2 = list.rstrip("\n")
-        result.append(temp2)
-    return result
+        result.append(temp2)"""
+    return new_lines
 
 
 def remove_duplicate_sentences(list):
@@ -34,13 +34,32 @@ def remove_duplicate_sentences(list):
      
     return result
 
+def process_list(input_list):
+    result = []
+    current_sentence = ""
 
+    for item in input_list:
+        # Remove leading/trailing whitespace and check if the item is a number
+        cleaned_item = item.strip()
+        if not cleaned_item.isdigit():
+            current_sentence += cleaned_item + ' '
+        elif current_sentence:
+            result.append(current_sentence)
+            current_sentence = ""
+
+    # Append the last sentence if it exists
+    if current_sentence:
+        result.append(current_sentence)
+
+    return result
 
 # 특정 패턴을 지정합니다.
-pattern = r'\d\n|\d{2}\n|\d{3}\n|(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3}\n)|\n'
+pattern = r'(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3}\n)|\n'
 directory = 'transcript'
 for filename in os.listdir(directory):
     f = os.path.join(directory,filename)
     transcripts = get_transcript_txt(f,pattern)
-    list_to_txt(remove_duplicate_sentences(transcripts),f'after_transcript/purified_{filename}')
+    list_to_txt(remove_duplicate_sentences(process_list(transcripts)),f'after_transcript/purified_{filename}')
+    #print(transcripts)
+    #list_to_txt(transcripts,f'after_transcript/purified_{filename}')
 
