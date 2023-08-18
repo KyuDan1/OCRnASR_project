6986ae6117a6f860d6763c329309a38bc6c050da
seungminBae <136676026+seungminBae@users.noreply.github.com>
2023-08-18 18:44:31 +0900

    Create WER_test
    created a new file - WER_test
     - calculates Word Error Rate
    modified existing files
     - handling empty; split_wav_by_transcript_timing creates no empty .wav file for wrong intervals

---
+++ WER_test.py
@@ -0,0 +1,33 @@
+import natsort
+import os
+from jiwer import wer
+
+
+def WER_of(out_dir, str_file):
+    def file_to_text(file):
+        with open(file, "r") as f:
+            return f.read()
+
+    outputs = natsort.natsorted(os.listdir(out_dir))
+    output = map(file_to_text, outputs)
+
+    with open(str_file, "r") as f:
+        strs = [x for x in f.read().split("\n\n") if x != "**ERROR**"]
+
+    return wer(" ".join(strs), output)
+
+
+def mean_WER_of(out_dir_list, str_file_list):
+    sum = 0
+    cnt = 0
+    for o, s in zip(out_dir_list, str_file_list):
+        sum += WER_of(o, s)
+        cnt += 1
+    return sum / cnt
+
+
+# example
+out_dir = "pure_ASR_transcript/audio_1-Orientation"
+str_file = "after_transcript/purified_번역- 1. Orientation.txt"
+print(WER_of(out_dir, str_file))
+print(mean_WER_of(os.listdir("pure_ASR_transcript"), os.listdir("after_transcript")))
--- extract_purified_transcript.py
+++ extract_purified_transcript.py
@@ -1,38 +1,69 @@
 import os
 import re
+import datetime
+
 
 def get_lines_list(text_path):
-    with open(text_path, 'r') as f:
+    with open(text_path, "r") as f:
         lines = f.readlines()
     return lines
 
-def list_to_txt(input_list, filename):
-    with open(filename, 'w') as file:
-        for item in input_list:
-            file.write(str(item) + '\n\n')
 
-    
+def list_to_txt(input_list, filename):
+    with open(filename, "w") as file:
+        for item in input_list:
+            file.write(str(item) + "\n\n")
+
+
+"""
 def get_transcript_txt(input_file, pattern):
-    new_lines =[]
+    new_lines = []
     result = []
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         lines = f.readlines()
     for text in lines:
         if not re.fullmatch(pattern, text):
             new_lines.append(text)
-    """for list in new_lines:
-        temp2 = list.rstrip("\n")
-        result.append(temp2)"""
+#   for list in new_lines:
+#       temp2 = list.rstrip("\n")
+#       result.append(temp2)
     return new_lines
+"""
+
+
+def get_transcript_txt(input_file):
+    def time_valid(text):
+        format = "%H:%M:%S,%f"
+        from_time = datetime.datetime.strptime(text[0, 12] + "000", format)
+        to_time = datetime.datetime.strptime(text[-13, -1] + "000", format)
+        return from_time <= to_time
+
+    new_lines = []
+    result = []
+    with open(input_file, "r") as f:
+        paragraphs = f.read().split("\n\n")
+    for paragraph in paragraphs:
+        if paragraph == "":
+            continue
+        lines = paragraph.split("\n")
+        new_lines.append("")
+        if time_valid(lines[1]):
+            new_lines.append(lines[1:])
+        else:
+            new_lines.append("**ERROR**")
+    return new_lines[1:]
 
 
 def remove_duplicate_sentences(list):
-    result = []
-    result.append(list[0])
-    for i in range(len(list)-1):
-        result.append(list[i+1].replace(list[i],""))
-     
+    result = list.copy()
+    for i in reversed(range(len(list) - 1)):
+        if i == 0:
+            break
+        l = len(result[i - 1])
+        if result[i][0:l] == result[i - 1]:
+            result[i] = result[i][l:]
     return result
+
 
 def process_list(input_list):
     result = []
@@ -42,7 +73,7 @@ def process_list(input_list):
         # Remove leading/trailing whitespace and check if the item is a number
         cleaned_item = item.strip()
         if not cleaned_item.isdigit():
-            current_sentence += cleaned_item + ' '
+            current_sentence += cleaned_item + " "
         elif current_sentence:
             result.append(current_sentence)
             current_sentence = ""
@@ -53,13 +84,17 @@ def process_list(input_list):
 
     return result
 
-# 특정 패턴을 지정합니다.
-pattern = r'(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3}\n)|\n'
-directory = 'transcript'
-for filename in os.listdir(directory):
-    f = os.path.join(directory,filename)
-    transcripts = get_transcript_txt(f,pattern)
-    list_to_txt(remove_duplicate_sentences(process_list(transcripts)),f'after_transcript/purified_{filename}')
-    #print(transcripts)
-    #list_to_txt(transcripts,f'after_transcript/purified_{filename}')
 
+# 특정 패턴을 지정합니다.
+pattern = r"(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3}\n)|\n"
+directory = "transcript"
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # transcripts = get_transcript_txt(f, pattern)
+    transcripts = get_transcript_txt(f)
+    list_to_txt(
+        remove_duplicate_sentences(transcripts),
+        f"after_transcript/purified_{filename}",
+    )
+    # print(transcripts)
+    # list_to_txt(transcripts,f'after_transcript/purified_{filename}')
