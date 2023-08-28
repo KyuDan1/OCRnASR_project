a6ae55131379deef2836a244564c116d60cc8cfc
seungminBae <136676026+seungminBae@users.noreply.github.com>
2023-08-28 19:33:49 +0900

    Reduced File I/O - (2)
    영어로 쓰기 귀찮으니 한국말로 하겠습니다.
    
    extract_purified_transcript.py:
     - 중복된 문장이 삭제되지 않는 문제를 해결했습니다.
     - 시간 정보 확인 등의 기능에서 실행 속도를 대폭 올렸습니다.
     - get_transcript_txt_with_time_extraction(): 기존 get_transcript_txt() 함수를 강화한 새로운 함수입니다. 시간 정보를 확인하면서 시간 정보 파일을 작성합니다. (extracting_time_info.py 대체)

--- extract_purified_transcript.py
+++ extract_purified_transcript.py
@@ -1,6 +1,56 @@
 import os
 import re
 
+error_code = "**ERROR**"
+specialcharacters = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+]
+
+time_format = [
+    "0",
+    "0",
+    ":",
+    "0",
+    "0",
+    ":",
+    "0",
+    "0",
+    ",",
+    "0",
+    "0",
+    "0",
+]
+
 
 def get_lines_list(text_path):
     with open(text_path, "r") as f:
@@ -10,8 +60,7 @@ def get_lines_list(text_path):
 
 def list_to_txt(input_list, filename):
     with open(filename, "w") as file:
-        for item in input_list:
-            file.write(str(item) + "\n\n")
+        file.write("\n".join(input_list))
 
 
 """
@@ -32,46 +81,128 @@ def get_transcript_txt(input_file, pattern):
 
 def get_transcript_txt(input_file):
     def time_valid(text):
-        def to_time(time_str):
-            h, m, s, ms = map(int, time_str.replace(",", ":").split(":"))
-            return h * 60 * 60 * 1000 + m * 60 * 1000 + s * 1000 + ms
+        def format_test(str):
+            if (
+                list(
+                    map(
+                        lambda x: x
+                        if x in specialcharacters
+                        else "0"
+                        if x.isdigit()
+                        else "1",
+                        str,
+                    )
+                )
+                != time_format
+            ):
+                return False
+            h, m, s, ms = map(int, str.replace(",", ":").split(":"))
+            if (m >= 60) or (s >= 60):
+                return False
+            # valid
+            return True
 
-        if text.replace("\t", "") == "":
-            return
-        print("!:", text)
-        from_time = to_time(text[0:12])
-        to_time = to_time(text[-12:])
-        return from_time <= to_time
+        text = text.strip()
+        if len(text) != 29:
+            return False
+        if not (
+            (text[0:12] <= text[-12:])
+            and format_test(text[0:12])
+            and format_test(text[0:12])
+        ):
+            return False
+        # valid
+        return True
 
     new_lines = []
-    result = []
     print(input_file)
     with open(input_file, "r") as f:
-        paragraphs = f.read().split("\n\n")
+        paragraphs = f.read().lower().split("\n\n")
     for paragraph in paragraphs:
         if paragraph == "":
             continue
-        lines = [x for x in paragraph.split("\n") if x != ""]
+        lines = [x.strip() for x in paragraph.split("\n") if x != ""]
         if not lines[0].isdigit():
             continue
-        new_lines.append("")
-        if time_valid(lines[1]):
-            new_lines.append("\n".join(lines[2:]))
+        ext_time = lines[1].strip()
+        if time_valid(ext_time):
+            new_lines.append(" ".join(lines[2:]))
         else:
-            new_lines.append("**ERROR**")
-    return new_lines[1:]
+            new_lines.append(error_code)
+
+    return new_lines
+
+
+def get_transcript_txt_with_time_extraction(input_file, time_filename):
+    def time_valid(text):
+        def format_test(str):
+            if (
+                list(
+                    map(
+                        lambda x: x
+                        if x in specialcharacters
+                        else "0"
+                        if x.isdigit()
+                        else "1",
+                        str,
+                    )
+                )
+                != time_format
+            ):
+                return False
+            h, m, s, ms = map(int, str.replace(",", ":").split(":"))
+            if (m >= 60) or (s >= 60):
+                return False
+            # valid
+            return True
+
+        text = text.strip()
+        if len(text) != 29:
+            return False
+        if not (
+            (text[0:12] <= text[-12:])
+            and format_test(text[0:12])
+            and format_test(text[0:12])
+        ):
+            return False
+        # valid
+        return True
+
+    ext_times = ""
+    new_lines = []
+    print(input_file)
+    with open(input_file, "r") as f:
+        paragraphs = f.read().lower().split("\n\n")
+    for paragraph in paragraphs:
+        if paragraph == "":
+            continue
+        lines = [x.strip() for x in paragraph.split("\n") if x != ""]
+        if not lines[0].isdigit():
+            continue
+        ext_time = lines[1].strip()
+        if time_valid(ext_time):
+            ext_times += "\n" + ext_time
+            new_lines.append(" ".join(lines[2:]))
+        else:
+            new_lines.append(error_code)
+
+    open(time_filename, "w").write(ext_times)
+
+    return new_lines
 
 
 def remove_duplicate_sentences(list):
     result = list.copy()
-    for i in reversed(range(len(list) - 1)):
-        if i == 0:
-            break
-        l = len(result[i - 1])
-        if result[i][0:l] == result[i - 1]:
-            result[i] = result[i][l:]
-        if result[0] == "\n":
-            result = result[1:]
+    i = len(list)
+    while i >= 2:
+        i = i - 1
+        str0 = result[i - 1]
+        str1 = result[i]
+        if str0 == error_code:
+            continue
+        l = len(str0)
+        if str1[0:l] == str0:
+            result[i] = str1[l:].strip()
     return result
 
 
@@ -96,66 +227,34 @@ def process_list(input_list):
 
 
 def del_punct(text):
-    specialcharacters = [
-        "!",
-        '"',
-        "#",
-        "$",
-        "%",
-        "&",
-        "'",
-        "(",
-        ")",
-        "*",
-        "+",
-        ",",
-        "-",
-        ".",
-        "/",
-        ":",
-        ";",
-        "<",
-        "=",
-        ">",
-        "?",
-        "@",
-        "[",
-        "\\",
-        "]",
-        "^",
-        "`",
-        "{",
-        "|",
-        "}",
-        "~",
-    ]
     return "".join(char for char in text if char not in specialcharacters)
 
 
-def lowering(text):
-    return text.lower()
-
-
+"""
 def remove_duplicate_sentences(sentences):
     unique_sentences = []
     for sentence in sentences:
         if sentence not in unique_sentences:
             unique_sentences.append(sentence)
     return unique_sentences
+"""
 
-
-# 특정 패턴을 지정합니다.
-pattern = r"(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3}\n)|\n"
-directory = "transcript"
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # transcripts = get_transcript_txt(f, pattern)
-    transcripts = get_transcript_txt(f)
-    list_to_txt(
-        remove_duplicate_sentences(
-            del_punct(lowering(transcript)) for transcript in transcripts
-        ),
-        f"after_transcript/purified_{filename}",
-    )
-    # print(transcripts)
-    # list_to_txt(transcripts,f'after_transcript/purified_{filename}')
+if __name__ == "__main__":
+    # 특정 패턴을 지정합니다.
+    # pattern = r"(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3}\n)|\n"
+    directory = "transcript"
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # transcripts = get_transcript_txt(f, pattern)
+        # transcripts = get_transcript_txt(f)
+        transcripts = get_transcript_txt_with_time_extraction(
+            f, f"time_info/time_infor_{filename}"
+        )
+        list_to_txt(
+            remove_duplicate_sentences(
+                [del_punct(transcript) for transcript in transcripts]
+            ),
+            f"after_transcript/purified_{filename}",
+        )
+        # print(transcripts)
+        # list_to_txt(transcripts,f'after_transcript/purified_{filename}')
